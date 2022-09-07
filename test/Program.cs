cacece3f6b43bb5298e20073a97f83ee01364b11
ramiz14 <ramizng@code.edu.az>
2022-09-07 23:26:04 +0400

    Add project files.

---
+++ test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Task-2
+            Console.WriteLine("Metni daxil edin");
+            string input = Console.ReadLine();
+            Console.WriteLine(ReverseArr(input));
+            #endregion
+        }
+
+        #region Task-2
+        static string ReverseArr(string input)
+        {
+            StringBuilder str = new StringBuilder();
+            string[] arr = input.Split(" ");
+            char[] chrArr = { };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                chrArr = arr[i].ToCharArray();
+                Array.Reverse(chrArr);
+                str.Append(chrArr);
+
+                if (i != arr.Length - 1)
+                {
+                    str.Append(' ');
+                }
+            }
+            return str.ToString();
+        }
+         #endregion
+    }
+}
